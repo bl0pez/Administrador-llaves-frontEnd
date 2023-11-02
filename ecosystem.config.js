deb3518da9b2b2e5caefc0a71442df7749cd35ba
bryan lopez <blopez4434@gmail.com>
2023-11-02 06:59:21 -0300

    add config eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,17 +9,4 @@ module.exports = {
             VITE_URL_BACKEND: 'http://localhost:3000/api/v2'
         }
     }],
-
-    deploy: {
-        production: {
-            user: 'SSH_USERNAME',
-            host: 'SSH_HOSTMACHINE',
-            ref: 'origin/master',
-            repo: 'GIT_REPOSITORY',
-            path: 'DESTINATION_PATH',
-            'pre-deploy-local': '',
-            'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-            'pre-setup': ''
-        }
-    }
 };
