0c0991c8532a752f800e44d34ce509ba277b48db
bryan lopez <blopez4434@gmail.com>
2023-11-02 06:53:29 -0300

    add config eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,25 @@
 module.exports = {
-    script: "serve",
-    env: {
-        PM2_SERVE_PATH: '.',
-        PM2_SERVE_PORT: 8080,
-        VITE_URL_BACKEND_PUBLIC_URL: 'http://localhost:3000/uploads/',
-        VITE_URL_BACKEND: 'http://localhost:3000/api/v2'
+    apps: [{
+        name: 'administrador-llaves-frontend',
+        script: 'serve',
+        env: {
+            PM2_SERVE_PATH: './dist',
+            PM2_SERVE_PORT: 3001,
+            VITE_URL_BACKEND_PUBLIC_URL: 'http://localhost:3000/uploads/',
+            VITE_URL_BACKEND: 'http://localhost:3000/api/v2'
+        }
+    }],
+
+    deploy: {
+        production: {
+            user: 'SSH_USERNAME',
+            host: 'SSH_HOSTMACHINE',
+            ref: 'origin/master',
+            repo: 'GIT_REPOSITORY',
+            path: 'DESTINATION_PATH',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
     }
-}
+};
