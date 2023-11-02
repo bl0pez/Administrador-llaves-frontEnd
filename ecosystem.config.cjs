f0dd2f1d495854a4feee19756a80aace290a2e5b
bryan lopez <blopez4434@gmail.com>
2023-11-02 07:08:19 -0300

    add config eco

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
         name: 'administrador-llaves-frontend',
         script: 'serve',
         env: {
-            PM2_SERVE_PATH: './dist',
+            PM2_SERVE_PATH: 'dist',
             PM2_SERVE_PORT: 3001,
             VITE_URL_BACKEND_PUBLIC_URL: 'http://localhost:3000/uploads/',
             VITE_URL_BACKEND: 'http://localhost:3000/api/v2'
