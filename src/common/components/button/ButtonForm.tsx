669d20e3bfaee74482610a4ad7c5027ac0360778
bryan lopez <blopez4434@gmail.com>
2024-01-27 09:23:28 -0300

    Refactor ButtonForm component

--- src/common/components/button/ButtonForm.tsx
+++ src/common/components/button/ButtonForm.tsx
@@ -1,38 +1,51 @@
-import { FC } from 'react';
-import { Button, CircularProgress, SvgIcon, SvgIconTypeMap } from '@mui/material';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { FC } from "react";
+import {
+  Button,
+  CircularProgress,
+  SvgIcon,
+  SvgIconTypeMap,
+} from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
 type ButtonFormProps = {
-    type: 'submit' | 'button' | 'reset';
-    title: string;
-    isLoading: boolean;
-    icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
-        muiName: string;
-    }
-    [x: string]: any;
-}
+  type: "submit" | "button" | "reset";
+  title: string;
+  isLoading: boolean;
+  icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+    muiName: string;
+  };
+  [x: string]: any;
+};
 
-export const ButtonForm:FC<ButtonFormProps> = ({ icon, isLoading, title, type, ...props }) => {
+export const ButtonForm: FC<ButtonFormProps> = ({
+  icon,
+  isLoading,
+  title,
+  type,
+  ...props
+}) => {
   return (
     <Button
-    variant='contained'
-    color='primary'
-    type={type}
-    sx={{
+      variant="contained"
+      color="primary"
+      type={type}
+      sx={{
         gap: 1,
-        height: '40px',
-        width: '100%',
-    }}
-    disabled={isLoading}
-    {...props}
+        height: "40px",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      disabled={isLoading}
+      {...props}
     >
-        {
-            isLoading 
-                ?  <CircularProgress  
-                    size={20}
-                /> : <SvgIcon component={icon} />
-        }
-            { title }
+      {isLoading ? (
+        <CircularProgress size={20} />
+      ) : (
+        <SvgIcon component={icon} />
+      )}
+      {title}
     </Button>
-  )
-}
+  );
+};
