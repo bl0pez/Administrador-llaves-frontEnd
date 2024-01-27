d73788273f94e701dcde566f73655b9d611e5b1c
bryan lopez <blopez4434@gmail.com>
2024-01-27 09:45:51 -0300

    Refactor MainModal component

--- src/common/components/modal/MainModal.tsx
+++ src/common/components/modal/MainModal.tsx
@@ -1,54 +1,48 @@
-import { FC } from 'react';
-import { Modal, Fade, Backdrop, Box } from '@mui/material';
-import { useThemeContext } from '@/theme/ThemeContextProvider';
+import { FC } from "react";
+import { Modal, Fade, Backdrop, Box } from "@mui/material";
+import { useThemeContext } from "@/theme/ThemeContextProvider";
 
 interface Props {
-    isOpen: boolean;
-    children: JSX.Element | JSX.Element[];
-    handleClose: () => void; 
+  isOpen: boolean;
+  children: JSX.Element | JSX.Element[];
+  handleClose: () => void;
 }
 
-export const MainModal: FC<Props> = ({ children, handleClose, isOpen}) => {
+export const MainModal: FC<Props> = ({ children, handleClose, isOpen }) => {
+  const { mode } = useThemeContext();
 
-    const { mode } = useThemeContext();
-
-    const style = {
-        minWidth: '300px',
-        with: '400px',
-        backgroundColor: mode === 'dark' ? '#424242' : '#fff',
-        zIndex: 9999,
-    };
+  const style = {
+    minWidth: "300px",
+    with: "400px",
+    backgroundColor: mode === "dark" ? "#424242" : "#fff",
+    zIndex: 9999,
+  };
 
   return (
     <Modal
-        open={isOpen}
-        sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backdropFilter: 'blur(4px)'
-        }}
-        onClose={handleClose}
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
+      open={isOpen}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backdropFilter: "blur(4px)",
+      }}
+      onClose={handleClose}
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
         backdrop: {
-            timeout: 200,
+          timeout: 200,
         },
-        }}
+      }}
     >
-        <Fade in={isOpen}>
-            <Box 
-                borderRadius={2}
-                boxShadow={4}
-                border={1}
-                sx={style}
-            >
-            {children}
-            </Box>
-        </Fade>
+      <Fade in={isOpen}>
+        <Box borderRadius={2} boxShadow={4} border={1} sx={style}>
+          {children}
+        </Box>
+      </Fade>
     </Modal>
-  )
-}
+  );
+};
